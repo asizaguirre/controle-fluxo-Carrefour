faf7e4a7485d6ba90a5e082f8c8929c989a5a191
Alam Izaguirre <asizaguirre@gmail.com>
2025-07-24 16:45:44 -0300

    Inicial commit - adicionando documentação de arquitetura

---
+++ src/main/java/com/asizaguirre/controlefluxocarrefour/ControleFluxoCarrefourApplication.java
@@ -0,0 +1,13 @@
+package com.asizaguirre.controlefluxocarrefour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleFluxoCarrefourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleFluxoCarrefourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asizaguirre/controlefluxocarrefour/ControleFluxoCarrefourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asizaguirre.controlefluxocarrefour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleFluxoCarrefourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
